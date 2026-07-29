56e4fee6985e9c68cd2fd711af77bbb4097f24b6
Aleksei Zalessky <aleksei.zalessky@mail.ru>
2026-07-29 21:38:08 +0300

    д/з 32

--- src/main/java/ru/otus/java/basic/homeworks/lesson32/HttpServer.java
+++ src/main/java/ru/otus/java/basic/homeworks/lesson32/HttpServer.java
@@ -3,29 +3,36 @@ package ru.otus.java.basic.homeworks.lesson32;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class HttpServer {
     private int port;
     private Dispatcher dispatcher;
+    private ExecutorService executor;
 
     public HttpServer(int port) {
         this.port = port;
         this.dispatcher = new Dispatcher();
+        this.executor = Executors.newFixedThreadPool(3);
     }
 
-    public void start(){
-        try( ServerSocket serverSocket = new ServerSocket(port);){
+    public void start() {
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
 
-            System.out.println("Server started on port " + port);
+            System.out.println("Сервер запущен на порту " + port);
 
             while (true) {
                 Socket socket = serverSocket.accept();
 
-                new Thread(() -> {
+                executor.execute(() -> {
                     try (Socket s = socket) {
                         System.out.println("Получено подключение");
                         byte[] buffer = new byte[8192];
                         int n = s.getInputStream().read(buffer);
+                        if (n < 0) {
+                            return;
+                        }
                         String rawRequest = new String(buffer, 0, n);
                         HttpRequest request = new HttpRequest(rawRequest);
                         request.info(true);
@@ -33,7 +40,7 @@ public class HttpServer {
                     } catch (IOException e) {
                         e.printStackTrace();
                     }
-                }).start();
+                });
             }
         } catch (IOException e) {
             e.printStackTrace();
